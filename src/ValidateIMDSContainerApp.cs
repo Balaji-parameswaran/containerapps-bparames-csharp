b68681b01bc2db55346ab91bd116f7a1daf91fca
Balaji Parameswaran <balajipar@microsoft.com>
2023-08-01 14:25:55 -0700

    Try .net6 code

--- src/ValidateIMDSContainerApp.cs
+++ src/ValidateIMDSContainerApp.cs
@@ -1,4 +1,4 @@
-using System;
+/* using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Net.Http;
@@ -94,3 +94,4 @@ namespace IMDSValidation
         }
     }
 }
+*/
