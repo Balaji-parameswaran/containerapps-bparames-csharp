fbad6767c0776ecf090eb8183fb509debfcc551f
Balaji Parameswaran <balajipar@microsoft.com>
2023-08-01 17:24:21 -0700

    Add using to make use of the class

--- src/Program.cs
+++ src/Program.cs
@@ -1,3 +1,5 @@
+using IMDSValidation
+
 var builder = WebApplication.CreateBuilder();
 
 // Add services to the container.
@@ -52,7 +54,7 @@ app.MapGet("/imds", async context =>
             {
                 // Get the second element as the bearer token
                 var bearerToken = parts[1];
-
+                ValidateIMDSContainerApp validateIMDSContainerApp = new ValidateIMDSContainerApp(bearerToken);
                 // Use the bearer token as needed
                 await context.Response.WriteAsync("There is a bearer token - bearer token: " + bearerToken);
             }
